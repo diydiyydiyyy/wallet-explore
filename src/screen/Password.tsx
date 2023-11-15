86d9fd90048407503beaf464b12d1f37e9fa9ed2
diydiyydiyyy <chalimatus.sadiyah.098@gmail.com>
2023-11-15 13:33:59 +0700

    create page password

--- src/screen/Password.tsx
+++ src/screen/Password.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function Password() {
   return (
     <View style={styles.container}>
-      <Text>Password!</Text>
+      <Text>Input Password :</Text>
+      <TextInput placeholder='Input Password' style={styles.input} />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Create Wallet</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +20,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 8,
+    paddingHorizontal: 32,
+    paddingVertical: 16
+    
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginTop: 10,
+    borderRadius: 8,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    textTransform: 'none',
   },
 });
